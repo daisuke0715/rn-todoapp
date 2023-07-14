816291e88902c0b44d36d2eb69ee192da8b0b29d
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2023-07-14 23:45:07 +0900

    detoxの初期設定時に生成されるstart.test.jsのE2Eテストが通るようにApp.tsxの実装を修正しました

--- App.tsx
+++ App.tsx
@@ -1,117 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [displayHelloText, setDisplayHelloText] = useState<boolean>(false);
+  const [displayWorldText, setDisplayWorldText] = useState<boolean>(false);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text testID="welcome" style={styles.text}>
+          {'Welcome to React Native!'}
+        </Text>
+        <View style={styles.section}>
+          <TouchableOpacity
+            testID="hello_button"
+            style={styles.button}
+            onPress={() => setDisplayHelloText(true)}>
+            <Text style={styles.text}>{'Show World!!!'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            testID="world_button"
+            style={styles.button}
+            onPress={() => setDisplayWorldText(true)}>
+            <Text style={styles.text}>{'Show World!!!'}</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <View style={styles.section}>
+          {displayHelloText && <Text style={styles.text}>{'Hello!!!'}</Text>}
+          {displayWorldText && <Text style={styles.text}>{'World!!!'}</Text>}
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  section: {
+    flexDirection: 'row',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    margin: 10,
+    padding: 10,
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
 
